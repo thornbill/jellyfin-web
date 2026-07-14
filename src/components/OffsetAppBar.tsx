328d3a7fe6b09a8e88698e710a256bf3babc526b
Bill Thornton <thornbill@users.noreply.github.com>
2026-07-14 15:25:49 -0400

    Hide the AppBar while scrolling

--- src/components/OffsetAppBar.tsx
+++ src/components/OffsetAppBar.tsx
@@ -1,4 +1,6 @@
 import AppBar, { type AppBarProps } from '@mui/material/AppBar';
+import Slide from '@mui/material/Slide';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import React, { useLayoutEffect, useRef, useState, type FC, type PropsWithChildren } from 'react';
 
@@ -7,12 +9,27 @@ const DEFAULT_APP_BAR_HEIGHT = 64;
 /** The height of a dense AppBar. */
 const DENSE_APP_BAR_HEIGHT = 48;
 
+interface HideOnScrollProps {
+    children: React.ReactElement;
+}
+
 interface OffsetAppBarProps extends AppBarProps {
     /** Use the dense variant of the AppBar, which has a smaller default height. */
     dense?: boolean;
     /** The elevation to apply when the user has scrolled. Defaults to 1. */
     elevation?: number;
 }
+
+/** Component that hides its children on small screens when the user scrolls down. */
+const HideOnScroll: FC<HideOnScrollProps> = ({ children }) => {
+    const isSmallScreen = useMediaQuery(t => t.breakpoints.up('sm'));
+    const trigger = useScrollTrigger();
+    return isSmallScreen ? children : (
+        <Slide appear={false} direction='down' in={!trigger}>
+            {children}
+        </Slide>
+    );
+};
 
 /**
  * AppBar wrapper with a fixed position and a spacer to prevent content from rendering underneath.
@@ -63,15 +80,17 @@ const OffsetAppBar: FC<PropsWithChildren<OffsetAppBarProps>> = ({
 
     return (
         <>
-            <AppBar
-                {...props}
-                ref={appBarRef}
-                position='fixed'
-                color={scrollTrigger ? 'default' : 'transparent'}
-                elevation={scrollTrigger ? elevation : 0}
-            >
-                {children}
-            </AppBar>
+            <HideOnScroll>
+                <AppBar
+                    {...props}
+                    ref={appBarRef}
+                    position='fixed'
+                    color={scrollTrigger ? 'default' : 'transparent'}
+                    elevation={scrollTrigger ? elevation : 0}
+                >
+                    {children}
+                </AppBar>
+            </HideOnScroll>
             {/* Spacer to prevent content rendering under the AppBar */}
             <div
                 aria-hidden='true'
